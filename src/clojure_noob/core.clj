8b37005103840cb28230ef761bb1cda7734e56e9
henriquesb6 <henriquesb6@hotmail.com>
2016-04-16 23:11:21 -0300

    infix function

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -131,3 +131,22 @@
     (if (empty? restkey)
       (update hash firstkey func)
       (assoc hash firstkey (my-update-in (get hash firstkey) restkey func)))))
+
+(def my-name (list 'read-string "(println \"Henrique Barros\")"))
+
+(def my-name2 (list 'println "Henrique Barros"))
+
+(defn infix
+  "Function that takes a list in the format (x + y * z - w)
+    and transforms it into a list that clojure can evaluate"
+  [string]
+  (list
+    (nth string 5)
+    (list
+      (nth string 1)
+      (nth string 0)
+      (list
+        (nth string 3)
+        (nth string 2)
+        (nth string 4)))
+     (nth string 6)))
